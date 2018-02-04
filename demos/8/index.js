ffc032d76be597be667a00ceb6f623abad3e30dc
dustinpfister <dustin.pfister@gmail.com>
2018-02-03 22:55:52 -0500

    using index.js files for demos

--- demos/8/index.js
+++ demos/8/index.js
@@ -4,7 +4,7 @@ module.exports = function (conf, req, res) {
 
     return new Promise(function (resolve,reject) {
 
-        resolve('so yeah query strings');
+        resolve('yeah I can do something here');
 
     });
 
