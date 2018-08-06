53d4a0da082ee9dcdf4a30b61d4a4d82f86fa283
dustinpfister <dustin.pfister@gmail.com>
2018-08-06 08:34:09 -0400

    opening file with fs.open, and fs.read

--- demos/8/readline-text-edit/open-file.js
+++ demos/8/readline-text-edit/open-file.js
@@ -4,13 +4,13 @@ path = require('path');
 //Buffer = require('buffer');
 
 // module vars
-//let fileBuff = Buffer.alloc(64);
+let fileBuff = Buffer.alloc(64);
 
-console.log(Buffer.alloc);
+//console.log(Buffer.alloc);
 
 
 // ensure the given absolute path to a file
-// is there. This will open a file for appending, and
+// is there. This will open a file for reading/appending, and
 // create the file if it is not there using fs.open
 // if all goes well it will resolve with an fd that can
 // be used to read and write to the file
@@ -18,7 +18,8 @@ exports.ensureFile = (filePath) => {
 
     return new Promise((resolve, reject) => {
 
-        fs.open(filePath, 'a',(err, fd) => {
+        // a+ is the append/read flag
+        fs.open(filePath, 'a+', (err, fd) => {
 
             if (err) {
 
@@ -41,20 +42,17 @@ exports.open = (filePath) => {
 
     this.ensureFile(filePath).then((fd) => {
 
-	/*
-	fs.read(fd, fileBuff,0,fileBuff.length,0,function(err,data){
-		
-		console.log(err);
-		console.log(data);
-		
-		
-	});
-	*/
+        fs.read(fd, fileBuff, 0, fileBuff.length, 0, function (err, bytesRead, buff) {
+
+            console.log(err);
+            console.log(buff.toString());
+
+        });
 
     }).catch ((err) => {
 
         console.log(err);
 
-    })
+    });
 
 }
