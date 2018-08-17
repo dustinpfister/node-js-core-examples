a64dc3cf0047f88b4069d3dfdae963e325416a6d
dustinpfister <dustin.pfister@gmail.com>
2018-08-17 14:31:46 -0400

    events demo

--- demos/8/filesystem-create-write-stream/event_onerror.js
+++ demos/8/filesystem-create-write-stream/event_onerror.js
@@ -1,11 +1,13 @@
 let fs = require('fs');
 
+// a writer in 'wx+' mode that will fail if the file
+// all ready exists
 let writer = fs.createWriteStream('test.txt',{flags:'wx+'})
 
-    .on('error', function (err) {
+.on('error', function (err) {
 
-        console.log(err);
+    console.log(err);
 
-    });
+});
 
 writer.write('this will fail if the file is there before hand');
--- demos/8/filesystem-create-write-stream/events.js
+++ demos/8/filesystem-create-write-stream/events.js
@@ -1,11 +1,42 @@
 let fs = require('fs');
 
-let writer = fs.createWriteStream('test.txt',{flags:'wx+'})
+let writer = fs.createWriteStream('test.txt', {
+        flags: 'w'
+    })
+
+    // the open event is fired when the file is ready
+    .on('open', function () {
+
+        console.log('open');
+
+    })
+
+    .on('drain', function () {
+
+        console.log('drain');
+
+    })
 
     .on('error', function (err) {
 
-        console.log(err);
+        console.log(err.message);
+
+    })
+
+    .on('close', function () {
+
+        console.log('close');
+
+    })
+
+    .on('finish', function () {
+
+        console.log('finish');
 
     });
 
 writer.write('this will fail if the file is there before hand');
+
+//writer.write(65); // attempting to write a number (will cause an error)
+
+writer.end(); // end the stream ( will fire on close)
