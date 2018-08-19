065dba271ca841ef505249863f155afdf6416d6b
dustinpfister <dustin.pfister@gmail.com>
2018-08-19 13:01:25 -0400

    event_end clean up

--- demos/8/filesystem-create-read-stream/event_end.js
+++ demos/8/filesystem-create-read-stream/event_end.js
@@ -1,19 +1,8 @@
 let fs = require('fs');
 
-fs.createReadStream('README.md')
+fs.createReadStream('README.md',{autoClose:false})
 
-.on('open', function () {
-
-    // setting auto close to false
-    this.autoClose = false;
-
-})
-
-.on('data', function (chunk) {
-
-    console.log(chunk);
-
-})
+.on('data', function (chunk) {console.log(chunk);})
 
 // the end event is fired when 
 // there is no more data to read, but 
@@ -32,7 +21,5 @@ fs.createReadStream('README.md')
 })
 
 .on('close', function () {
-
     console.log('closed now');
-
 });
