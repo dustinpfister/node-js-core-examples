b9d4ace869a90ea033d1ba60490ce8dd7a8eb3c6
dustinpfister <dustin.pfister@gmail.com>
2018-08-19 11:52:36 -0400

    basic options example

--- demos/8/filesystem-create-read-stream/basic_options.js
+++ demos/8/filesystem-create-read-stream/basic_options.js
@@ -1,12 +1,11 @@
 let fs = require('fs'),
 reader = fs.createReadStream('README.md', {
-
-        flag: 'a+',
-		start: 8,
-		end: 64,
-        highWaterMark: 16
-
-    });
+    flag: 'a+',
+    encoding: 'ascii',
+    start: 8,
+    end: 64,
+    highWaterMark: 16
+});
 
 reader.on('data', function (chunk) {
 
