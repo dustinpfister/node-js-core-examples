b3ae889a39e2bf5ca71bede9ad9b6318dc877c4b
dustinpfister <dustin.pfister@gmail.com>
2018-08-19 13:43:43 -0400

    readable example clean up

--- demos/8/filesystem-create-read-stream/event_readable.js
+++ demos/8/filesystem-create-read-stream/event_readable.js
@@ -1,12 +1,13 @@
 let fs = require('fs');
 
-let reader = fs.createReadStream('README.md').pause();
+let reader = fs.createReadStream('README.md',{highWaterMark:16}).pause();
 
 reader.on('data', function(chunk){  console.log(chunk);})
 
+// readable event
 reader.on('readable', function () {
 
-    console.log('this is now readable');
+    console.log('ready to read');
 
     this.read();
 
