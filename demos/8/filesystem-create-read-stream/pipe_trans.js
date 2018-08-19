16b024d43ae404f8e3a7dcdb6105f67befc6015d
dustinpfister <dustin.pfister@gmail.com>
2018-08-19 10:28:44 -0400

    transform example

--- demos/8/filesystem-create-read-stream/pipe_trans.js
+++ demos/8/filesystem-create-read-stream/pipe_trans.js
@@ -2,26 +2,28 @@ let fs = require('fs'),
 stream = require('stream'),
 marked = require('marked'),
 
-
 trans = new stream.Transform({
 
-        write: function (chunk, enc, next) {
+        transform: function (chunk, enc, next) {
 
-            //this.push(chunk.toString());
+            this.push(chunk.toString());
 
             next(null);
+
+        },
+
+        final: function () {
+
+            fs.writeFile('README.html', marked(this.read().toString()), function () {
+
+                console.log('html file written');
+
+            });
 
         }
 
     });
-	
-	
+
 fs.createReadStream('README.md')
 
-.pipe(trans)
-
-.on('data', function(chunk){
-	
-	console.log(chunk);
-	
-})
+.pipe(trans);
