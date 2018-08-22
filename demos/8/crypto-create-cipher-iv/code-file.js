214ba6cf0e9b16823ebb09a37b2abca1e2eb2452
dustinpfister <dustin.pfister@gmail.com>
2018-08-22 12:44:44 -0400

    decode example working great

--- demos/8/crypto-create-cipher-iv/code-file.js
+++ demos/8/crypto-create-cipher-iv/code-file.js
@@ -8,13 +8,16 @@ key = Buffer.alloc(32), // key should be 32 bytes
 iv = Buffer.alloc(16), // iv should be 16
 
 filename = process.argv[2] || 'test.txt',
-passwd = process.argv[3] || '1234-spaceballs';
+passwd = process.argv[3] || '1234-spaceballs',
+outfile = path.basename(filename, path.extname(filename)) + '.coded';
 
 // make the key something other than a blank buffer
 key = Buffer.concat([Buffer.from(passwd)], key.length);
 
 // randomize the iv, for best results
-//iv = Buffer.from(Array.prototype.map.call(iv, () => {return Math.floor(Math.random() * 256)}));
+iv = Buffer.from(Array.prototype.map.call(iv, () => {
+            return Math.floor(Math.random() * 256)
+        }));
 
 // make the cipher with the current suite, key, and iv
 let cipher = crypto.createCipheriv(a, key, iv);
@@ -26,4 +29,14 @@ fs.createReadStream(filename)
 .pipe(cipher)
 
 // pipe to writer
-.pipe(fs.createWriteStream(path.basename(filename,path.extname(filename))+'.coded'));
+.pipe(fs.createWriteStream(outfile)
+
+    .on('close', function () {
+
+        console.log(outfile + ' has been coded with: ');
+        console.log('key:');
+        console.log(key.toString('hex'));
+        console.log('iv');
+        console.log(iv.toString('hex'));
+
+    }));
--- demos/8/crypto-create-cipher-iv/decode-file.js
+++ demos/8/crypto-create-cipher-iv/decode-file.js
@@ -4,17 +4,12 @@ fs = require('fs');
 
 // Cipher Suites, key, and the iv
 let a = 'aes-256-cbc',
-key = Buffer.alloc(32), // key should be 32 bytes
-iv = Buffer.alloc(16), // iv should be 16
+//key = Buffer.alloc(32), // key should be 32 bytes
+//iv = Buffer.alloc(16), // iv should be 16
 
 filename = process.argv[2] || 'test.coded',
-passwd = process.argv[3] || '1234-spaceballs';
-
-// make the key something other than a blank buffer
-key = Buffer.concat([Buffer.from(passwd)], key.length);
-
-// randomize the iv, for best results
-//iv = Buffer.from(Array.prototype.map.call(iv, () => {return Math.floor(Math.random() * 256)}));
+key = Buffer.from(process[3] || '313233342d737061636562616c6c730000000000000000000000000000000000','hex'),
+iv = Buffer.from(process[4] || '00000000000000000000000000000000','hex');
 
 // make the cipher with the current suite, key, and iv
 let cipher = crypto.createDecipheriv(a, key, iv);
