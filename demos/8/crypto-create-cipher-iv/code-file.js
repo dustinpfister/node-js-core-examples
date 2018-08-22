0216e4b77cc249923e7a26c29a585e9cf7bfc467
dustinpfister <dustin.pfister@gmail.com>
2018-08-22 14:27:21 -0400

    writing keys file

--- demos/8/crypto-create-cipher-iv/code-file.js
+++ demos/8/crypto-create-cipher-iv/code-file.js
@@ -8,8 +8,9 @@ key = Buffer.alloc(32), // key should be 32 bytes
 iv = Buffer.alloc(16), // iv should be 16
 
 filename = process.argv[2] || 'test.txt',
+bName = path.basename(filename, path.extname(filename)),
 passwd = process.argv[3] || '1234-spaceballs',
-outfile = path.basename(filename, path.extname(filename)) + '.coded';
+outfile = bName + '.coded';
 
 // make the key something other than a blank buffer
 key = Buffer.concat([Buffer.from(passwd)], key.length);
@@ -31,12 +32,35 @@ fs.createReadStream(filename)
 // pipe to writer
 .pipe(fs.createWriteStream(outfile)
 
-.on('close', function () {
+    .on('close', function () {
 
-    console.log(outfile + ' has been coded with: ');
-    console.log('key:');
-    console.log(key.toString('hex'));
-    console.log('iv');
-    console.log(iv.toString('hex'));
+        console.log(outfile + ' has been coded with: ');
+        console.log('key:');
+        console.log(key.toString('hex'));
+        console.log('iv');
+        console.log(iv.toString('hex'));
 
-}));
+        fs.writeFile(bName + '-keys.json', 
+
+           // this will be the json
+           JSON.stringify({
+                key: key.toString('hex'),
+                iv: iv.toString('hex')
+            }), 
+
+            // callback
+            function (e) {
+
+                if (e) {
+
+                    console.log('error writing keys.json');
+
+                } else {
+
+                    console.log('keys.josn written');
+
+                }
+
+        });
+
+    }));
