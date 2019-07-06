5698bd704263a0727610b25e90c20d35e74e923d
dustinpfister <dustin.pfister@gmail.com>
2019-07-06 11:07:46 -0400

    stream promise

--- demos/8/crypto-create-cipher/stream.js
+++ demos/8/crypto-create-cipher/stream.js
@@ -1,17 +1,35 @@
-let crypto = require('crypto'),
-cipher = crypto.createCipher('aes192', 'a password');
+let crypto = require('crypto');
 
-let encrypted = '';
-cipher.on('readable', () => {
-  const data = cipher.read();
-  if (data)
-    encrypted += data.toString('hex');
-});
-cipher.on('end', () => {
-  console.log(encrypted);
-  // Prints: ca981be48e90867604588e75d04feabb63cc007a8f8ad89b10616ed84d815504
-});
+let CryptIt = (text, algorithm, password) => {
+    let hex = '';
+    algorithm = algorithm || 'aes192';
+    password = password || '1234-spaceballs';
+    if (text === undefined) {
+        return Promise.reject('no text given');
+    }
+    return new Promise((resolve, reject) => {
+        let cipher = crypto.createCipher(algorithm, password);
+        cipher.on('readable', () => {
+            let data = cipher.read();
+            if (data) {
+                hex += data.toString('hex');
+            }
+        });
+        cipher.on('end', () => {
+            resolve(hex);
+        });
+        cipher.on('error', (e) => {
+            reject(e.message);
+        });
+        cipher.write(text);
+        cipher.end();
+    });
+};
 
-//cipher.write('some clear text data');
-cipher.write('hello there');
-cipher.end();
+CryptIt('hello there yes this is a stream.')
+.then((hex) => {
+    console.log(hex);
+})
+.catch ((e) => {
+    console.log(e);
+})
