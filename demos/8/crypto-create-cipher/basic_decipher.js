bcb3c09aa004c8c697631b1649818e626a28d92b
dustinpfister <dustin.pfister@gmail.com>
2019-07-06 10:25:49 -0400

    basic decipher clean up

--- demos/8/crypto-create-cipher/basic_decipher.js
+++ demos/8/crypto-create-cipher/basic_decipher.js
@@ -2,18 +2,11 @@ let crypto = require('crypto'),
 algorithm = 'aes192',
 password = process.argv[2] || 'password1234',
 cipher = crypto.createCipher(algorithm, password),
-decipher = crypto.createDecipher(algorithm, password);
-
-let hex = cipher.update('This is something I want encrypted', 'utf8', 'hex');
+decipher = crypto.createDecipher(algorithm, password),
+hex = cipher.update('This is something I want encrypted', 'utf8', 'hex');
 hex += cipher.final('hex');
 console.log(hex);
-
-// attempting to convert the hex to plain text will not
-// work.
-console.log(Buffer.from(hex, 'hex').toString('utf8'));
-// [encrypted string]
-
-// the hex must be deciphered
+// the hex can be deciphered in a similar way
 let str = decipher.update(hex, 'hex', 'utf8');
 str += decipher.final('utf8');
-console.log(str)
+console.log(str);
