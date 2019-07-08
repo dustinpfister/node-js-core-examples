f7e5b97cdbd4b2294a4a62a194ae206d7f84803f
dustinpfister <dustin.pfister@gmail.com>
2019-07-08 17:43:01 -0400

    pipe demo

---
+++ demos/8/pipe/stdin.js
@@ -0,0 +1,9 @@
+process.stdin.setEncoding('utf8');
+process.stdin.on('readable', () => {
+    let chunk;
+    // Use a loop to make sure we read all available data.
+    while ((chunk = process.stdin.read()) !== null) {
+        process.stdout.write(chunk);
+    }
+    process.stdin.end();
+});
