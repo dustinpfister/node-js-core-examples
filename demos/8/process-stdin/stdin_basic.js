9d89ac65abb96f0071eb30d9ffc6842c02f34711
dustinpfister <dustin.pfister@gmail.com>
2019-07-09 12:04:19 -0400

    clean up

--- demos/8/process-stdin/stdin_basic.js
+++ demos/8/process-stdin/stdin_basic.js
@@ -2,7 +2,7 @@ process.stdin.on('readable', () => {
     let chunk;
     while ((chunk = process.stdin.read()) !== null) {
         process.stdout.write('we have a chunk: \n');
-		console.log(chunk.length);
+        console.log(chunk.length);
         process.stdout.write(chunk);
     }
 });
