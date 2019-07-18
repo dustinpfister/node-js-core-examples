ab6adb2fa6c45603e824efc3136a2e48ab53583c
dustinpfister <dustin.pfister@gmail.com>
2019-07-18 14:14:06 -0400

    static example on request

--- demos/8/net/static.js
+++ demos/8/net/static.js
@@ -1,12 +1,22 @@
-const http = require('http');
+let http = require('http');
 
-const server = http.createServer((req, res) => {
-        res.end('hello world');
-    });
+let server = http.createServer();
+
+// on request
+server.on('request', (req, res) => {
+    console.log(req.url);
+    if(req.url === '/') {
+        res.end('root');
+    }
+    else {
+        res.end();
+    }
+});
+
 server.on('clientError', (err, socket) => {
     socket.end('HTTP/1.1 400 Bad Request\r\n\r\n');
 });
 server.listen(8080, 'localhost', 200, () => {
     let add = server.address();
-    console.log('static server up on http://' + add.address + ':' +add.port);
+    console.log('static server up on http://' + add.address + ':' + add.port);
 });
