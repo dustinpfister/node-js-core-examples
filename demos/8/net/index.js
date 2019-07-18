7b5e24478c43ca9085e448ac13c454077eeee966
dustinpfister <dustin.pfister@gmail.com>
2019-07-18 13:10:14 -0400

    index example working in telnet

--- demos/8/net/index.js
+++ demos/8/net/index.js
@@ -1,12 +1,18 @@
 let net = require('net');
 let server = net.createServer((socket) => {
-  socket.end('goodbye\n');
-}).on('error', (err) => {
-  // handle errors here
-  throw err;
-});
+        // 'connection' listener
+        console.log('client connected');
+        socket.on('end', () => {
+            console.log('client disconnected');
+        });
+        socket.write('hello\r\n');
+        socket.end()
+    }).on('error', (err) => {
+        // handle errors here
+        throw err;
+    });
 
 // listen on port 8080 on local host with a backlog of 200
 server.listen(8080, 'localhost', 200, () => {
-  console.log('opened server on', server.address());
+    console.log('opened server on', server.address());
 });
