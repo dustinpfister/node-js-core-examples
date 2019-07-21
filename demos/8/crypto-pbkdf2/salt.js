46d5758ee1354d711453e81c2af57bb8b02ae5ef
dustinpfister <dustin.pfister@gmail.com>
2019-07-21 11:19:52 -0400

    commnets

--- demos/8/crypto-pbkdf2/salt.js
+++ demos/8/crypto-pbkdf2/salt.js
@@ -1,19 +1,18 @@
 let crypto = require('crypto');
 
+// hast it function that just takes a 
+// password and a salt value with hard coded
+// values for everything else
 let hashIt = (pw, salt) => {
-
     let i = 10000,
     len = 16,
     digest = 'sha512';
-
     if (!pw) {
         return Promise.reject('invalid password');
     }
-
     if (!salt) {
         return Promise.reject('salt must be given');
     }
-
     return new Promise((resolve, reject) => {
         crypto.pbkdf2(pw, salt, i, len, digest, (e, key) => {
             if (e) {
@@ -26,19 +25,16 @@ let hashIt = (pw, salt) => {
             }
         });
     });
-
 };
 
+// the same password results in different keys
 hashIt('weak123', '10000000')
 .then((result) => {
-
-    console.log(result.key);
-
+    console.log(result.key.toString('hex'));
+    // c64152a63e3c35710ee22e6cf598801a
 });
-
 hashIt('weak123', 'pu77fbaz')
 .then((result) => {
-
-    console.log(result.key);
-
+    console.log(result.key.toString('hex'));
+    // fa5e869e389ff97f7a00bde481a91ae5
 });
