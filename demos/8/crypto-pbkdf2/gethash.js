936155ca591590a74db089d0028ef851e59b02c1
dustinpfister <dustin.pfister@gmail.com>
2019-07-20 20:30:53 -0400

    get hashes

--- demos/8/crypto-pbkdf2/gethash.js
+++ demos/8/crypto-pbkdf2/gethash.js
@@ -1,9 +1,19 @@
 let crypto = require('crypto');
 
 let sha_options = crypto.getHashes().filter((hash) => {
-
         return hash.match(/^sha/);
-
     });
-
 console.log(sha_options);
+/*[ 'sha',
+  'sha1',
+  'sha1WithRSAEncryption',
+  'sha224',
+  'sha224WithRSAEncryption',
+  'sha256',
+  'sha256WithRSAEncryption',
+  'sha384',
+  'sha384WithRSAEncryption',
+  'sha512',
+  'sha512WithRSAEncryption',
+  'shaWithRSAEncryption' ]
+*/
