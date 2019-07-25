cf3f7a4a596a7e292ce8d185851f6dd4f3f09efc
dustinpfister <dustin.pfister@gmail.com>
2019-07-24 21:34:54 -0400

    console folder

---
+++ demos/8/console/basic.js
@@ -0,0 +1,7 @@
+// the console.log method ca be called and passed
+// a value to be logged to the standard output like this
+console.log('hello world');
+
+// more than one item can be passed
+let a = 'foo', b = 'baz', c = 42;
+console.log(a, b, c);
