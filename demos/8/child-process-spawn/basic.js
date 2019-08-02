ae5dd4b24cf91c4f16668d46c52eac00f95d1469
dustinpfister <dustin.pfister@gmail.com>
2019-08-02 13:01:39 -0400

    commnets for basic example

--- demos/8/child-process-spawn/basic.js
+++ demos/8/child-process-spawn/basic.js
@@ -2,10 +2,12 @@
 // using spawn
 let spawn = require('child_process').spawn,
 
+// spawn test.js
 script = spawn('node', ['test.js']);
 
+// what to do for the standard output for
+// test.js launched via the node spawn
+// child process method
 script.stdout.on('data', function(data){
-
     console.log(data.toString());
-
 });
