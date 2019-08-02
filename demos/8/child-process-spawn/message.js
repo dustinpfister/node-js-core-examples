0304884c3ab103d8192f3552a4f90da64cb09595
dustinpfister <dustin.pfister@gmail.com>
2019-08-02 14:02:39 -0400

    messsage clean up

--- demos/8/child-process-spawn/message.js
+++ demos/8/child-process-spawn/message.js
@@ -1,20 +1,10 @@
-
-/*
-let spawn = require('child_process').spawn,
-script = spawn('node', ['message_test.js']);
- */
-
 let fork = require('child_process').fork,
 script = fork('message_test.js');
 
 script.on('message', (m) => {
-
     console.log(m);
-
 });
 
 script.on('close', () => {
-
     console.log('close');
-
-})
+});
--- demos/8/child-process-spawn/message_test.js
+++ demos/8/child-process-spawn/message_test.js
@@ -1,9 +1,1 @@
-console.log('this works of course');
-//console.log(process.send);
 process.send({mess: 'hello there'});
-
-setTimeout(() => {
-
-    console.log('what the hell');
-
-}, 5000);
