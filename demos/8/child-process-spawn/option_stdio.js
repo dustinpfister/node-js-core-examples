53dac9c53c6e9306e727a12666dd9275ecc6c3a2
dustinpfister <dustin.pfister@gmail.com>
2019-08-02 17:04:15 -0400

    clean up

--- demos/8/child-process-spawn/option_stdio.js
+++ demos/8/child-process-spawn/option_stdio.js
@@ -1,17 +1,17 @@
 let spawn = require('child_process').spawn,
-
 // set up io as ipc
-script = spawn('node', ['option_stdio_test.js'], {
-        stdio: ['ipc', 'pipe', 'pipe']
-    });
-
+options = {
+    stdio: ['ipc', 'pipe', 'pipe']
+},
+script = spawn('node', ['option_stdio_test.js'], options);
+// what to do when this parent process
+// receives a message from the node child process
 script.on('message', (m) => {
-
     console.log(m.b); // 12
     script.kill();
-
-})
-
+});
+// send an IPC message to the
+// child process
 script.send({
     a: 5
 });
