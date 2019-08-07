6f8959a12a04f6e31274c558b43391fb2f79b172
dustinpfister <dustin.pfister@gmail.com>
2019-08-07 11:26:37 -0400

    cleaned up basic-test

--- demos/8/child-process-fork/basic-test.js
+++ demos/8/child-process-fork/basic-test.js
@@ -1,11 +1,2 @@
-// the standard out
-process.stdout.write('hello node fork! : ');
-// and the standard in are inherited from
-// the parent process by default
-process.stdin.on('data', (data) => {
-    console.log(data.toString('hex'));
-});
-
-process.stdin.on('end', (data) => {
-    console.log('end of data');
-});
+// the standard out is inherited from the parent process
+process.stdout.write('hello node fork!');
