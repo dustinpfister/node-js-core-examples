3e4f28e6ce9e1411ee20bacc3d817588c0858896
dustinpfister <dustin.pfister@gmail.com>
2019-08-08 11:38:46 -0400

    stopIf method

--- demos/8/stream/read_random_letters.js
+++ demos/8/stream/read_random_letters.js
@@ -1,24 +1,40 @@
 let stream = require('stream');
 
+// default stop if method
+let stopIf = function (buff, opt) {
+    if (opt.byteIndex < opt.totalBytes) {
+        // use the push method to push data to be
+        // consumed
+        this.push(buff);
+    } else {
+        // push null when done
+        this.push(null);
+    }
+};
+
 // returns a RandomLetters Readable Stream
 exports.RandomLetters = function (opt) {
     opt = opt || {};
-    let byteCount = opt.startIndex === undefined ? 0: opt.startIndex,
-    totalBytes = opt.totalBytes === undefined ? 1024: opt.totalBytes;
+    opt.byteIndex = opt.byteIndex === undefined ? 0 : opt.byteIndex;
+    opt.totalBytes = opt.totalBytes === undefined ? 1024 : opt.totalBytes;
+    opt.stopIf = opt.stopIf || stopIf;
     return new stream.Readable({
         // must have a read method
         read: function () {
             let n = 65 + Math.round(25 * Math.random());
             let buff = Buffer.from(n.toString(16), 'hex');
+            /*
             if (byteCount < totalBytes) {
-                // use the push method to push data to be
-                // consumed
-                this.push(buff);
+            // use the push method to push data to be
+            // consumed
+            this.push(buff);
             } else {
-                // push null when done
-                this.push(null);
+            // push null when done
+            this.push(null);
             }
-            byteCount += 1;
+             */
+            opt.stopIf.call(this, buff, opt);
+            opt.byteIndex += 1;
         }
     });
 };
