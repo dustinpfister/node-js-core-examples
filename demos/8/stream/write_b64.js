538ff01524db07a6d26bc1ae82d35e79d5d10daa
dustinpfister <dustin.pfister@gmail.com>
2019-08-08 15:26:44 -0400

    write_b64 working okay

--- demos/8/stream/write_b64.js
+++ demos/8/stream/write_b64.js
@@ -1,16 +1,15 @@
 let stream = require('stream');
 
 let B64 = function () {
-
     return new stream.Writable({
         write: function (chunk, enc, cb) {
             console.log(chunk.toString('base64'));
             cb(null);
         }
     });
-
 };
 
 let writer = B64();
 
-writer.write('foobar');
+writer.write('So this will be based 64 encoded');
+//U28gdGhpcyB3aWxsIGJlIGJhc2VkIDY0IGVuY29kZWQ= 
