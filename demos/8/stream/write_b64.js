a2aaa0def0d969c6eed360d7dfd9c8685fb46c98
dustinpfister <dustin.pfister@gmail.com>
2019-08-08 15:42:01 -0400

    b64 back to text also

--- demos/8/stream/write_b64.js
+++ demos/8/stream/write_b64.js
@@ -1,15 +1,29 @@
 let stream = require('stream');
 
-let B64 = function () {
+let WB64 = function (opt) {
+    opt = opt || {};
+    opt.toB64 = opt.toB64 === undefined ? true : opt.toB64;
+    if (opt.toB64) {
+        return new stream.Writable({
+            write: function (chunk, enc, cb) {
+                console.log(chunk.toString('base64'));
+                cb(null);
+            }
+        });
+    }
     return new stream.Writable({
         write: function (chunk, enc, cb) {
-            console.log(chunk.toString('base64'));
+            console.log(Buffer.from(chunk.toString('utf8'), 'base64').toString());
             cb(null);
         }
     });
 };
 
-let writer = B64();
-
-writer.write('So this will be based 64 encoded');
-//U28gdGhpcyB3aWxsIGJlIGJhc2VkIDY0IGVuY29kZWQ= 
+let toB64 = WB64(),
+fromB64 = WB64({
+        toB64: false
+    });
+toB64.write('So this will be based 64 encoded');
+// U28gdGhpcyB3aWxsIGJlIGJhc2VkIDY0IGVuY29kZWQ=
+fromB64.write('U28gdGhpcyB3aWxsIGJlIGJhc2VkIDY0IGVuY29kZWQ=');
+// So this will be based 64 encoded
