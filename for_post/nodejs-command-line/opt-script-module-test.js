ab035f514f8b127a5082c5b4c426fac0896108fd
dustinpfister <dustin.pfister@gmail.com>
2019-09-15 07:46:08 -0400

    opt-script-module

---
+++ for_post/nodejs-command-line/opt-script-module-test.js
@@ -0,0 +1,3 @@
+let mod = require('./opt-script-module.js');
+
+console.log( mod(2, 40) );
--- for_post/nodejs-command-line/opt-script.js
+++ for_post/nodejs-command-line/opt-script.js
@@ -1,7 +1,6 @@
 let func = (a, b) => {
-
     return a + b;
-
 };
-
 console.log( func(40,2) );
+
+// $ node script
