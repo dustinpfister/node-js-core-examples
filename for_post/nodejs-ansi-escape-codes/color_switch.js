345489b1d3575836f263038a4006c386ed336b9b
dustinpfister <dustin.pfister@gmail.com>
2019-09-19 13:29:00 -0400

    color backgrounds

--- for_post/nodejs-ansi-escape-codes/color_switch.js
+++ for_post/nodejs-ansi-escape-codes/color_switch.js
@@ -5,23 +5,23 @@ let color = {
         bgNum = bgNum === undefined ? 47 : bgNum;
         return '\u001b[' + fgNum + 'm' + '\u001b[' + bgNum + 'm' + mess + '\u001b[39m\u001b[49m';
     },
-    black: (mess) => color.byNum(mess, 30),
-    red: (mess) => color.byNum(mess, 31),
-    green: (mess) => color.byNum(mess, 32),
-    yellow: (mess) => color.byNum(mess, 33),
-    blue: (mess) => color.byNum(mess, 34),
-    magenta: (mess) => color.byNum(mess, 35),
-    cyan: (mess) => color.byNum(mess, 36),
-    white: (mess) => color.byNum(mess, 37)
+    black: (mess, fgNum) => color.byNum(mess, 30, fgNum),
+    red: (mess, fgNum) => color.byNum(mess, 31, fgNum),
+    green: (mess, fgNum) => color.byNum(mess, 32, fgNum),
+    yellow: (mess, fgNum) => color.byNum(mess, 33, fgNum),
+    blue: (mess, fgNum) => color.byNum(mess, 34, fgNum),
+    magenta: (mess, fgNum) => color.byNum(mess, 35, fgNum),
+    cyan: (mess, fgNum) => color.byNum(mess, 36, fgNum),
+    white: (mess, fgNum) => color.byNum(mess, 37, fgNum)
 
 };
 
 let text = 'hello world.';
-console.log(color.black(text, 31));
-console.log(color.red(text, 31));
-console.log(color.green(text, 31));
-console.log(color.yellow(text, 31));
-console.log(color.blue(text, 31));
-console.log(color.magenta(text, 31));
-console.log(color.cyan(text, 31));
-console.log(color.white(text, 31));
+console.log(color.black(text));
+console.log(color.red(text));
+console.log(color.green(text));
+console.log(color.yellow(text));
+console.log(color.blue(text));
+console.log(color.magenta(text));
+console.log(color.cyan(text));
+console.log(color.white(text, 40));
