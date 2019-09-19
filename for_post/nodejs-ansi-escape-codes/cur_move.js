a23fde8779c79986721b4b4eaeb955c98eddd57d
dustinpfister <dustin.pfister@gmail.com>
2019-09-19 13:45:06 -0400

    cur_move example looks good

--- for_post/nodejs-ansi-escape-codes/cur_move.js
+++ for_post/nodejs-ansi-escape-codes/cur_move.js
@@ -1,8 +1,23 @@
 
 process.stdout.write('\u001b[47m');
 process.stdout.write('\u001b[30m');
-process.stdout.write('          ')
+process.stdout.write('..........\n');
+process.stdout.write('..........\n');
+process.stdout.write('..........\n');
+process.stdout.write('..........\n');
+process.stdout.write('\u001b[s');
+// move up 4 and back 5
+process.stdout.write('\u001b[4A');
 process.stdout.write('\u001b[5D@');
 
 // reset colors
 process.stdout.write('\u001b[39m\u001b[49m');
+// restore cursor
+process.stdout.write('\u001b[u');
+
+/*
+@.........
+..........
+..........
+..........
+*/
