1512bcdf4d0095e1b94dad9f9d26d9b4c5b4f8cf
dustinpfister <dustin.pfister@gmail.com>
2019-09-19 13:39:14 -0400

    cur_move

---
+++ for_post/nodejs-ansi-escape-codes/cur_move.js
@@ -0,0 +1,8 @@
+
+process.stdout.write('\u001b[47m');
+process.stdout.write('\u001b[30m');
+process.stdout.write('          ')
+process.stdout.write('\u001b[5D@');
+
+// reset colors
+process.stdout.write('\u001b[39m\u001b[49m');
