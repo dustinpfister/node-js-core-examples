f10f486f26b1006bb924b4d3d44861b4deb66762
dustinpfister <dustin.pfister@gmail.com>
2019-10-25 14:16:58 -0400

    updated filesystem-stat-isfile demo

--- demos/8/filesystem-stat-isfile/index.js
+++ demos/8/filesystem-stat-isfile/index.js
@@ -1,110 +1,97 @@
 
 let fs = require('fs'),
 path = require('path'),
-cwd = process.cwd(),
-dir_used = process.argv[2] || cwd;
+promisify = require('util').promisify,
+dir_root = process.argv[2] || process.cwd();
+
+// promisify fs.readdir and fs.stat
+let readdir = promisify(fs.readdir),
+stat = promisify(fs.stat),
+// is file method that returns an object for the file
+// of false for a dir
+isFile = (filePath) => {
+    return stat(filePath)
+    .then((stat) => {
+        if (stat.isFile()) {
+            return {
+                filePath: filePath,
+                stat: stat
+            }
+        }
+        return false;
+    });
+}
+
+readdir(dir_root)
+.then((files) => {
+    return Promise.all(files.map((file) => {
+            return isFile(path.join(dir_root, file));
+        }));
+})
+.then((result) => {
+    result = result.filter((item) => {
+            return item;
+        });
+    console.log(result);
+})
+.catch((e) => {
+    console.log(e.message);
+})
+
+/*
 
 let getDirList = function (dir) {
-
-    dir = dir || cwd;
-
-    return new Promise(function (resolve, reject) {
-
-        // read the contents of the dir
-        fs.readdir(dir, function (e, list) {
-
-            if (e) {
-
-                reject(e.message);
-
-            }
-
-            resolve(list);
-
-        });
-
-    });
-
+dir = dir || cwd;
+return new Promise(function (resolve, reject) {
+// read the contents of the dir
+fs.readdir(dir, function (e, list) {
+if (e) {
+reject(e.message);
+}
+resolve(list);
+});
+});
 };
 
+// list only files
 let listFiles = function (dir) {
-
-    let i = 0,
-    len = 0,
-    files = [];
-
-    dir = dir || dir_used;
-
-    return new Promise(function (resolve, reject) {
-
-        getDirList(dir).then(function (list) {
-
-            let i = 0,
-            len = list.length,
-
-            next = function () {
-
-                let file = list[i];
-
-                fs.stat(path.join(dir, file), function (e, stat) {
-
-                    //console.log(file);
-                    //console.log(stat.isFile());
-
-                    if (e) {
-
-                        reject(e.message);
-
-                    }
-
-                    if (stat.isFile()) {
-
-                        files.push(file);
-
-                    }
-
-                    i += 1;
-
-                    if (i >= len) {
-
-                        resolve(files);
-
-                    } else {
-
-                        next();
-
-                    }
-
-                });
-
-            };
-
-            next();
-
-            //});
-
-        });
-
-    });
-
+let i = 0,
+len = 0,
+files = [];
+dir = dir || dir_used;
+return new Promise(function (resolve, reject) {
+getDirList(dir).then(function (list) {
+let i = 0,
+len = list.length,
+next = function () {
+let file = list[i],
+filePath = path.join(dir, file);
+fs.stat(filePath, function (e, stat) {
+if (e) {
+reject(e.message);
+}
+if (stat.isFile()) {
+files.push({
+path: filePath,
+fileName: file,
+stat: stat
+});
+}
+i += 1;
+if (i >= len) {
+resolve(files);
+} else {
+next();
+}
+});
+};
+next();
+});
+});
 };
 
 listFiles(dir_used).then(function (list) {
-
-    console.log(list);
-
+console.log(list);
 });
 
-/*
-fs.stat(path.join(cwd, 'README.md'), function (e, stats) {
-
-if (e) {
-
-console.log(e);
-
-}
-
-console.log(stats.isFile());
-
-});
 */
