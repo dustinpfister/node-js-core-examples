57366741a7aa44fdf31055d6b47e8d2e58c319bc
dustinpfister <dustin.pfister@gmail.com>
2019-11-18 12:01:07 -0500

    basic example comments

--- for_post/nodejs-promise/basic.js
+++ for_post/nodejs-promise/basic.js
@@ -6,6 +6,8 @@ let readFile = (filePath, encoding, flag) => {
     if (!filePath) {
         return Promise.reject(new Error('file path not given to readFile method.'))
     }
+    // return a new Promise created with the
+    // promise constructor
     return new Promise((resolve, reject) => {
         fs.readFile(filePath, {
             encoding: encoding,
@@ -22,12 +24,11 @@ let readFile = (filePath, encoding, flag) => {
 
 readFile('./README.md')
 .then((data) => {
-
+    // the content of the file if it is there
+    // and all id well
     console.log(data.toString());
-
 })
 .catch((e) => {
-
+    // error message if there is a problem
     console.log(e.message);
-
 });
