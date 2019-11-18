e9ac916a660b294dc6a119a8945e4d449458088f
dustinpfister <dustin.pfister@gmail.com>
2019-11-18 14:46:40 -0500

    making a single map file

--- for_post/nodejs-promise/promise_all.js
+++ for_post/nodejs-promise/promise_all.js
@@ -1,10 +1,12 @@
 let fs = require('fs'),
+path = require('path'),
 promisify = require('util').promisify;
 
-let mkdir = promisify(fs.mkdir);
+let mkdir = promisify(fs.mkdir),
+writeFile = promisify(fs.writeFile);
 
+// make the maps folder
 let mkMapsFolder = () => {
-
     // attempt to make a new maps folder
     return mkdir('./maps')
     // if successful just resolve
@@ -23,9 +25,45 @@ let mkMapsFolder = () => {
     });
 };
 
+// write a map file
+let writeMapFile = (opt) => {
+
+    opt = opt || {};
+    opt.name = opt.name || '';
+    opt.root = opt.root || path.resolve('./');
+    opt.fileName = opt.fileName || 'map_' + opt.name + '.json';
+    opt.width = opt.width || 10;
+    opt.height = opt.height || 10;
+    // create cells
+    let i = 0,
+    cells = [],
+    len = opt.width * opt.height;
+    while (i < len) {
+        cells.push({
+            type: 'blank'
+        });
+        i += 1;
+    }
+    // create map object
+    let map = {
+        name: opt.name,
+        width: opt.width,
+        height: opt.height,
+        cells: cells
+    };
+    // write map
+    return writeFile(path.join(opt.root, opt.fileName), JSON.stringify(map), 'utf8');
+
+};
+
 mkMapsFolder()
 .then(() => {
     console.log('all is good with maps folder, creating maps.');
+	return writeMapFile({
+		root: './maps',
+		name: 1
+		
+	});
 })
 .catch((e) => {
     console.log(e.message);
