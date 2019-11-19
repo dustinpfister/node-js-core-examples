7d33afb004689a4de730df42fad9dba7b2076042
dustinpfister <dustin.pfister@gmail.com>
2019-11-19 14:18:05 -0500

    eol hard coded

--- for_post/nodejs-os/eol.js
+++ for_post/nodejs-os/eol.js
@@ -3,3 +3,9 @@ let os = require('os');
 console.log(Buffer.from(os.EOL).toString('hex'));
 // '0d0a' if 'win32'
 // '0a' if posix
+
+
+console.log(Buffer.from('\r\n').toString('hex'));
+// '0d0a'
+console.log(Buffer.from('\n').toString('hex'));
+// '0a'
