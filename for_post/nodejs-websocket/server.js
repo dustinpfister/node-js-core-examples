a25fba1be5e6432d174fa114236d436d71f22401
dustinpfister <dustin.pfister@gmail.com>
2019-11-20 16:56:33 -0500

    server_ws,js

--- for_post/nodejs-websocket/server.js
+++ for_post/nodejs-websocket/server.js
@@ -76,65 +76,22 @@ webServer.listen(port, function () {
     console.log('port: ' + port);
 });
 
+// using my simple web socket lib
+let sws = require('./server_ws.js');
+sws({
+    port: 8095,
+    host: 'localhost',
+    onReady: function (api) {
+        api.sendText('Yeah this works!');
+        api.sendText('I can send text');
+        api.socket.end();
+    }
+});
+
+
 /*
 wsServer resources
-
 https://developer.mozilla.org/en-US/docs/Web/API/WebSockets_API/Writing_WebSocket_servers
-
 accept key example
 https://github.com/theturtle32/WebSocket-Node/blob/master/lib/WebSocketRequest.js
-
  */
-
-// The Web Socket server
-let wsServer = http.createServer();
-let crypto = require('crypto');
-
-// generate the accept key for the upgrade request
-let genAcceptKey = (req) => {
-    let key = req.headers['sec-websocket-key'],
-    sha1 = crypto.createHash('sha1');
-    sha1.update(key + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11');
-    let acceptKey = sha1.digest('base64');
-    return acceptKey;
-};
-// accept upgrade handler for upgrade event
-let acceptUpgrade = (req, socket) => {
-    // gen accept key
-    let acceptKey = genAcceptKey(req);
-    // write response
-    socket.write('HTTP/1.1 101 Web Socket Protocol Handshake\r\n' +
-        'Upgrade: WebSocket\r\n' +
-        'Connection: Upgrade\r\n' +
-        'Sec-WebSocket-Accept: ' + acceptKey + '\r\n' +
-        '\r\n');
-};
-
-// simple send text frame helper
-let sendTextFrame = function (socket, text) {
-    let firstByte = 0x00,
-    secondByte = 0x00,
-    payloadLength = Buffer.from([0, 0]),
-    payload = Buffer.alloc(text.length);
-    payload.write(text);
-    firstByte |= 0x80; // fin bit true
-    firstByte |= 0x01; // opt code of 1 (text)
-    secondByte |= text.length; // mask and payload len
-    let frame = Buffer.concat([Buffer.from([firstByte]), Buffer.from([secondByte]), payload]);
-    socket.write(frame);
-};
-
-// upgrade handler
-wsServer.on('upgrade', (req, socket, head) => {
-
-    // accept upgrade
-    acceptUpgrade(req, socket);
-
-    // send simple text frame
-    sendTextFrame(socket, 'Hello');
-
-});
-
-wsServer.listen(wsPort, () => {
-    console.log('web socket server is up on port: ' + wsPort);
-});
