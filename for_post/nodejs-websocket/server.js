ae0d0c85464c3e9e176d2684ceeff9cd1b8b7439
dustinpfister <dustin.pfister@gmail.com>
2019-11-20 14:52:17 -0500

    genAcceptKey helper

--- for_post/nodejs-websocket/server.js
+++ for_post/nodejs-websocket/server.js
@@ -78,21 +78,21 @@ webServer.listen(port, function () {
 
 // The Web Socket server
 let wsServer = http.createServer();
-
 let crypto = require('crypto');
+
+// generate the accept key for the upgrade request
+let genAcceptKey = (req) => {
+    let key = req.headers['sec-websocket-key'],
+    sha1 = crypto.createHash('sha1');
+    sha1.update(key + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11');
+    let acceptKey = sha1.digest('base64');
+    return acceptKey;
+};
 
 wsServer.on('upgrade', (req, socket, head) => {
 
     console.log('upgrade request');
-    //console.log(head.length);
-    let key = req.headers['sec-websocket-key'];
-
-    let sha1 = crypto.createHash('sha1');
-    sha1.update(key + '258EAFA5-E914-47DA-95CA-C5AB0DC85B11');
-    let acceptKey = sha1.digest('base64');
-
-    console.log('sec-websocket-key: ', key);
-    console.log('sec-accept-key: ', acceptKey);
+    let acceptKey = genAcceptKey(req);
 
     socket.write('HTTP/1.1 101 Web Socket Protocol Handshake\r\n' +
         'Upgrade: WebSocket\r\n' +
