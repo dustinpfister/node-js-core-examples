9f6f73cfb3df92b048ddf49209ba96e7a74609bd
dustinpfister <dustin.pfister@gmail.com>
2019-11-20 12:35:51 -0500

    client system

---
+++ for_post/nodejs-websocket/public/client.js
@@ -0,0 +1,30 @@
+let socket = new WebSocket("ws://localhost:8090/");
+
+socket.onopen = function (e) {
+    console.log('okay we opened a connection');
+    socket.send('foobar');
+};
+
+socket.onmessage = function (e) {
+    console.log('we have a message');
+    console.log(e.data.toString());
+};
+
+socket.onclose = function (e) {
+    console.log('socket closed')
+    if (e.wasClean) {
+        console.log('clean close');
+        console.log(e.code);
+        console.log(e.reason);
+    } else {
+        console.log('conection died');
+        console.log(e.code);
+    }
+};
+
+// an on error handler can be defined
+// but the error object contains to message
+socket.onerror = function (e) {
+    console.log(e.constructor.name); // Event
+    console.log(e.message); // undefined
+};
