5c6c17324408bdf3787609afc7e42c8eb3a491fb
dustinpfister <dustin.pfister@gmail.com>
2019-11-21 16:56:07 -0500

    nodejs-events

---
+++ for_post/nodejs-event/event_class.js
@@ -0,0 +1,31 @@
+let Emitter = require('events'),
+events = new Emitter();
+
+events.on('update', (obj) => {
+    console.log(obj.i)
+});
+
+events.on('overload', (obj) => {
+    obj.i %= obj.iMax;
+});
+
+let obj = {
+    i: 0,
+    iMax: 10
+};
+
+let updateObject = function (obj) {
+
+    obj.i += 1;
+
+    if (obj.i > obj.iMax) {
+        events.emit('overload', obj);
+    }
+    events.emit('update', obj);
+};
+
+setInterval(function () {
+
+    updateObject(obj);
+
+}, 250);
