14397ac7e6e6a8e1aeb303835267719db760b66e
dustinpfister <dustin.pfister@gmail.com>
2019-11-22 18:12:45 -0500

    count example

--- for_post/nodejs-filesystem-unlink/example_count.js
+++ for_post/nodejs-filesystem-unlink/example_count.js
@@ -6,19 +6,22 @@ deleteFile = promisify(fs.unlink),
 writeFile = promisify(fs.writeFile),
 readFile = promisify(fs.readFile);
 
-command = path.resolve(process.argv[2] || 'step');
+command = path.resolve(process.argv[2] || 'step'),
+filePath = path.join(process.cwd(), 'count.json');
 
-let step = (filePath) => {
+let stepCount = () => {
     let state = {};
     return readFile(filePath)
     .then((json) => {
         state = JSON.parse(json);
+        state.mess = 'count updated: ' + new Date();
         state.count += 1;
         return writeFile(filePath, JSON.stringify(state), 'utf8');
     })
     .catch((e) => {
         if (e.code === 'ENOENT') {
             state = {
+                mess: 'new count created: ' + new Date(),
                 count: 1
             };
             let json = JSON.stringify(state);
@@ -32,10 +35,36 @@ let step = (filePath) => {
     });
 };
 
-step(path.join(process.cwd(), 'count.json'))
+let deleteCount = () => {
+    return deleteFile(filePath)
+    .then(() => {
+        return Promise.resolve({
+            count: 0,
+            mess: 'count file deleted'
+        });
+    })
+}
+
+let Run = (command) => {
+    command = command || 'step';
+    if (command === 'step') {
+        return stepCount();
+    }
+    if (command === 'kill') {
+        return deleteCount();
+    }
+    return Promise.reject(new Error('unknown command'));
+};
+
+Run(process.argv[2])
+
 .then((state) => {
+    console.log('mess: ' + state.mess);
     console.log('count: ' + state.count);
 })
+
 .catch((e) => {
+
     console.log(e);
+
 });
