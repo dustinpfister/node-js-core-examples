12873b75094ec44363685e52bb1f3179f237d797
dustinpfister <dustin.pfister@gmail.com>
2019-11-25 10:08:49 -0500

    write_file_open

---
+++ for_post/nodejs-filesystem/write_file_open.js
@@ -0,0 +1,29 @@
+let fs = require('fs'),
+path = require('path'),
+promisify = require('util').promisify,
+cwd = process.cwd(),
+open = promisify(fs.open),
+close = promisify(fs.close),
+read = promisify(fs.read),
+write = promisify(fs.write),
+
+path_file = path.join(cwd, 'db.txt');
+
+open(path_file, 'w+', 0o666)
+
+.then((fd) => {
+
+
+    console.log(fd);
+    return close(fd);
+
+})
+
+.catch((e) => {
+
+    console.log('\n', 'Error: ');
+    console.log('code:' + e.code);
+    console.log('mess: ' + e.message);
+    console.log('');
+
+});
