e34d774ef287b788a3474cacfa7003207d7d12b0
dustinpfister <dustin.pfister@gmail.com>
2019-11-25 13:22:20 -0500

    nodejs-filesystem-open

---
+++ for_post/nodejs-filesystem-open/write_file_open.js
@@ -0,0 +1,51 @@
+let fs = require('fs'),
+path = require('path'),
+promisify = require('util').promisify,
+cwd = process.cwd(),
+open = promisify(fs.open),
+stat = promisify(fs.stat),
+close = promisify(fs.close),
+write = promisify(fs.write),
+
+path_file = path.join(cwd, 'db.txt');
+
+let fd = null,
+fileByteLength = 0;
+
+// get stats
+stat(path_file)
+.then((stat) => {
+    fileByteLength = stat.size;
+})
+.catch((e) => {
+    if (e.code === "ENOENT") {
+        fileByteLength = 0;
+        return Promise.resolve();
+    }
+    return Promise.reject(e);
+})
+// open
+open(path_file, 'a', 0o666)
+// write
+.then((nFd) => {
+    fd = nFd;
+    console.log('writing to file of size: ' + fileByteLength);
+    // buffer
+    let buff = Buffer.from('foo', 'utf8'),
+    buff_start = 0,
+    buff_end = buff.length,
+    // byte start position
+    startPosition = fileByteLength;
+    // using fs.write
+    return write(fd, buff, buff_start, buff_end, startPosition);
+})
+// close
+.then(() => {
+    return close(fd);
+})
+.catch((e) => {
+    console.log('\n', 'Error: ');
+    console.log('code:' + e.code);
+    console.log('mess: ' + e.message);
+    console.log('');
+});
