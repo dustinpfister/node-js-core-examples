8e67928aaaa615245cd9211177c93c0a226832a7
dustinpfister <dustin.pfister@gmail.com>
2019-11-26 10:09:23 -0500

    basic_callback

---
+++ for_post/nodejs-filesystem-open/basic_callback.js
@@ -0,0 +1,30 @@
+let fs = require('fs'),
+path = require('path'),
+
+path_file = path.join(process.cwd(), 'db.txt');
+
+fs.open(path_file, 'a+', 0o666, (err, fd) => {
+
+    if (err) {
+        console.log(err);
+        console.log(fd);
+    } else {
+        let buff = Buffer.from('Hello World', 'utf8'),
+        buff_start = 0,
+        buff_length = buff.length,
+        file_pos = 0;
+        fs.write(fd, buff, buff_start, buff_length, file_pos, (err) => {
+            if (err) {
+                console.log(err);
+                fs.close(fd, () => {
+                    console.log('file closed');
+                });
+            } else {
+                console.log('write done')
+                fs.close(fd, () => {
+                    console.log('file closed');
+                });
+            }
+        });
+    }
+});
