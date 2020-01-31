02ac6326da3db37dbce35f00e57c258e0fb4166f
dustinpfister <dustin.pfister@gmail.com>
2020-01-31 15:15:48 -0500

    node paths folder

---
+++ for_post/nodejs-paths/dirname.js
@@ -0,0 +1,5 @@
+let path = require('path');
+
+let dir = path.dirname('./foo/man/chew.js');
+console.log(dir);
+// './foo/man'
---
+++ for_post/nodejs-paths/filename.js
@@ -0,0 +1,5 @@
+let path = require('path');
+
+let fileName = path.basename('./foo/man/chew.js');
+console.log(fileName);
+// returns 'chew.js'
