7aca86ca3fd8191687daf7c8599bf3fe0a855e88
dustinpfister <dustin.pfister@gmail.com>
2020-02-06 12:27:40 -0500

    content type is html

--- for_post/nodejs-example-web-server-starting-point/server_static.js
+++ for_post/nodejs-example-web-server-starting-point/server_static.js
@@ -11,21 +11,33 @@ let server = http.createServer();
 
 server.on('request', function (req, res) {
 
-    console.log(req.method); // the type of request (GET, POST, HEAD, ect)
-    console.log(req.url); // the path to the resource
+    let encoding = 'binary',
+    resource = path.join(dir_publicHTML, req.url),
+    resource_ext = path.extname(resource);
 
-    let encoding = 'utf8';
-    readFile(path.join(dir_publicHTML, req.url), encoding)
+    // if no file extension append 'index.html'
+    resource = !resource_ext ? path.join(resource, 'index.html') : resource;
+    resource_ext = !resource_ext ? '.html' : resource_ext;
+
+    if (resource_ext === '.html') {
+        encoding = 'utf8';
+    }
+
+    console.log('');
+    console.log('req.method: ', req.method); // the type of request (GET, POST, HEAD, ect)
+    console.log('req.url: ', req.url); // the path to the resource
+    console.log('resource: ', resource); // the resource in the public folder
+    console.log('encdoing: ', encoding); // encoding
+
+    readFile(resource, encoding)
 
     .then((fileData) => {
 
-        
         res.writeHead(200, {
-        'Content-Type': 'text/plain'
+            'Content-Type': 'text/html'
         });
         res.write(fileData);
         res.end();
-        
 
     })
 
