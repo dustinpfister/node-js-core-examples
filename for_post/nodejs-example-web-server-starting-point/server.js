1f218395da8c189fc8d87f8c3456982a4134624d
dustinpfister <dustin.pfister@gmail.com>
2020-02-06 10:51:50 -0500

    nodejs-example-web-server-starting-point folder

---
+++ for_post/nodejs-example-web-server-starting-point/server.js
@@ -0,0 +1,19 @@
+let http = require('http'),
+
+port = process.env.port || process.argv[2] || 8080;
+
+let server = http.createServer();
+
+server.on('request', function (req, res) {
+    res.writeHead(200, {
+        'Content-Type': 'text/plain'
+    });
+    res.write('Hello Node.js World!');
+    res.end();
+});
+
+server.listen(port, () => {
+
+    console.log('web server is up on port: ' + port);
+
+});
