0a94ead3db783cc89b3bbd2f1ebbba1edadff91c
dustinpfister <dustin.pfister@gmail.com>
2020-02-06 15:44:14 -0500

    comments

--- for_post/nodejs-example-web-server-starting-point/server_paths.js
+++ for_post/nodejs-example-web-server-starting-point/server_paths.js
@@ -6,20 +6,19 @@ port = process.env.port || process.argv[2] || 8080;
 let server = http.createServer();
 
 server.on('request', function (req, res) {
-
     let dir_pathmod = path.join(__dirname, 'paths', req.url, 'index.js');
-
+    // try to get the module for the current path if it is there
     try {
         let pathMod = require(dir_pathmod);
         pathMod(req, res);
     } catch (e) {
+        // send an error message if something goes wrong
         res.writeHead(501, {
             'Content-Type': 'text/plain'
         });
         res.write('501: ' + e.message);
         res.end();
     }
-
 });
 
 server.listen(port, () => {
