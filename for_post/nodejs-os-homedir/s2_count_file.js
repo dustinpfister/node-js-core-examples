2ec17e244a927979db5c0efcfe0a93d8462baebd
dustinpfister <dustin.pfister@gmail.com>
2020-05-23 18:51:40 -0400

    count example

--- for_post/nodejs-os-homedir/s2_count_file.js
+++ for_post/nodejs-os-homedir/s2_count_file.js
@@ -26,9 +26,14 @@ let getUserCount = () => {
     });
 };
 
+let putUserCount = (obj) => {
+    let path_count = path.join(dir_home, '.count.json');
+    return writeFile(path_count, JSON.stringify(obj));
+};
+
 getUserCount()
 .then((obj) => {
-
     console.log(obj.count);
-
-})
+    obj.count += 1;
+    return putUserCount(obj);
+});
