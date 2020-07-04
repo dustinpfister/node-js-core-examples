75a298c42baa8a2cdc92b81882edb1e1e4276e34
dustinpfister <dustin.pfister@gmail.com>
2020-07-04 07:32:35 -0400

    comments

--- for_post/nodejs-module/s1_module_exports/point.js
+++ for_post/nodejs-module/s1_module_exports/point.js
@@ -1,10 +1,9 @@
+// private helper method
 let parseAxis = (a) => {
-
     return a === undefined || typeof a != 'number' || String(a) === 'NaN' ? 0 : a;
-
 };
 
-// a Main method
+// a Main public method
 let Point = (x, y) => {
     return {
         x: parseAxis(x),
@@ -12,7 +11,7 @@ let Point = (x, y) => {
     };
 };
 
-// a static method
+// an additional static public method
 Point.distance = (pt1, pt2) => {
     return Math.sqrt(Math.pow(pt1.x - pt2.x, 2) + Math.pow(pt1.y - pt2.y, 2));
 };
