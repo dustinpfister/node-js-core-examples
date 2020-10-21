881b4dbff3f2f708c5715f1006608245431b4f87
dustinpfister <dustin.pfister@gmail.com>
2020-10-21 16:20:11 -0400

    clean up

--- for_post/nodejs-child-process-exec/basic.js
+++ for_post/nodejs-child-process-exec/basic.js
@@ -1,18 +1,14 @@
-
-// using exec
 let exec = require('child_process').exec,
-
 script = exec('git status');
-
+// what to do for data coming from the standard out
 script.stdout.on('data', function(data){
     console.log(data.toString());
 });
-
+// what to do with data coming from the standard error
 script.stderr.on('data', function(data){
     console.log(data.toString());
 });
-
-
+// what to do when the command is done
 script.on('exit', function(code){
     console.log('program ended with code: ' + code);
 });
