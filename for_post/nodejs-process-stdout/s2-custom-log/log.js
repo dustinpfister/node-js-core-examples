245769c0869cea1e3dd9624bb7abea7fdb25c27f
dustinpfister <dustin.pfister@gmail.com>
2021-03-18 16:05:12 -0400

    colors object

--- for_post/nodejs-process-stdout/s2-custom-log/log.js
+++ for_post/nodejs-process-stdout/s2-custom-log/log.js
@@ -1,21 +1,28 @@
 let colorCode = {
+  black: '\u001b[30m',
   red: '\u001b[31m',
+  green: '\u001b[32m',
+  orange: '\u001b[33m',
+  blue: '\u001b[34m',
+  purple: '\u001b[35m',
+  cyan: '\u001b[36m',
+  white: '\u001b[37m',
   reset: '\u001b[39m'
 };
 
-var fullLog = (mess, type, eol, out, err, color) => {
+var fullLog = (mess, type, eol, out, err, colors) => {
     mess = mess || '';
     type = type || 'info';
     eol = eol || '';
     out = out === undefined ? process.stdout : out;
     err = err === undefined ? process.stderr : err;
-    color = color || false;
+    colors = colors || false;
     if(type === 'info'){
-        if(color){
-           out.write(colorCode.red);
+        if(colors){
+           out.write(colorCode[colors.info]);
         }
         out.write(mess + eol);
-        if(color){
+        if(colors){
            out.write(colorCode.reset);
         }
     }
@@ -26,7 +33,10 @@ var fullLog = (mess, type, eol, out, err, color) => {
 
 // typically log function
 var api = function(mess, type, eol){
-   fullLog(mess, type, eol, process.stdout, process.stderr, true);
+   fullLog(mess, type, eol, process.stdout, process.stderr, {
+       info: 'cyan',
+       error: 'red'
+   });
 };
 
 // typical settings for clean output
