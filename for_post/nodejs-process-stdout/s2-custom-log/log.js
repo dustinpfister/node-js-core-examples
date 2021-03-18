2e00cf1e03bc007d9586c958e47fc152a42308ad
dustinpfister <dustin.pfister@gmail.com>
2021-03-18 15:55:40 -0400

    more advanced log.js

--- for_post/nodejs-process-stdout/s2-custom-log/log.js
+++ for_post/nodejs-process-stdout/s2-custom-log/log.js
@@ -1,13 +1,33 @@
-module.exports = (mess, type, eol, out, err) => {
+let colorCode = {
+  red: '\u001b[31m',
+  reset: '\u001b[39m'
+};
+
+var fullLog = (mess, type, eol, out, err, color) => {
     mess = mess || '';
     type = type || 'info';
     eol = eol || '';
     out = out === undefined ? process.stdout : out;
     err = err === undefined ? process.stderr : err;
+    color = color || false;
     if(type === 'info'){
+        if(color){
+           out.write(colorCode.red);
+        }
         out.write(mess + eol);
+        if(color){
+           out.write(colorCode.reset);
+        }
     }
     if(type === 'error'){
         err.write(mess + eol);
     }
 };
+
+var api = function(mess, type, eol){
+   fullLog(mess, type, eol, process.stdout, process.stderr, true);
+};
+
+api.fullLog = fullLog;
+
+module.exports = api;
