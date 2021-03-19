7e20fd53f1b94f4783fa14d1e80892b887588c9c
dustinpfister <dustin.pfister@gmail.com>
2021-03-19 14:52:40 -0400

    commnets for basic example

--- for_post/nodejs-buffer-slice/basic.js
+++ for_post/nodejs-buffer-slice/basic.js
@@ -4,3 +4,5 @@ let buff = Buffer.from('afbfcf', 'hex');
 let a = buff.slice(1, 3);
 
 console.log(a.toString('hex')); // bfcf
+// the source buffer is not changed
+console.log(buff.toString('hex')); // afbfcf
