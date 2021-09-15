45f6f89055a6bc7824f007c2904e4f0423c23696
dustinpfister <dustin.pfister@gmail.com>
2021-09-15 11:27:38 -0400

    better error handling

--- for_post/nodejs-http/s2-respond-to-get/basic-static-server.js
+++ for_post/nodejs-http/s2-respond-to-get/basic-static-server.js
@@ -1,18 +1,28 @@
 let http = require('http'),
+path = require('path'),
 fs = require('fs'),
 server = http.createServer(),
+// port, host, public dir
 port = 8080,
-host = 'localhost'
+host = 'localhost',
+dir_public = process.argv[0] || process.cwd();
+
 // on request
 server.on('request', (req, res) => {
     if (req.url === '/' && req.method === 'GET') {
-        let reader = fs.createReadStream('./public/index.html', {
+        let uri_index = path.join(__dirname, 'index.html');
+        let reader = fs.createReadStream(uri_index, {
                 highWaterMark: 128
             });
         res.setHeader('Content-Type', 'text/html');
         reader.on('data', (data) => {
             console.log('sent chunk: ')
             res.write(data);
+        });
+        reader.on('error', (e) => {
+            console.log(e.message)
+            res.write(e.message);
+            res.end();
         });
         reader.on('end', () => {
             console.log('done');
