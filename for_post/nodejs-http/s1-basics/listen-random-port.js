cf3577feabc427ea840adb58bc23a0d595754aeb
dustinpfister <dustin.pfister@gmail.com>
2021-09-15 11:08:36 -0400

    listen on random port

--- for_post/nodejs-http/s1-basics/listen-random-port.js
+++ for_post/nodejs-http/s1-basics/listen-random-port.js
@@ -1,11 +1,6 @@
 let http = require('http');
-
 let server = http.createServer();
-
-// what to do on listening event
-server.on('listening', () => {
+server.listen(0, () => {
     let address = server.address();
     console.log('listening on random port: ' + address.port);
 });
-
-server.listen(0);
