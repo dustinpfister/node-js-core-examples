e24028d674454560ab9797f336f649dbea982209
dustinpfister <dustin.pfister@gmail.com>
2021-10-04 15:38:25 -0400

    not even bothering with errors in basic example

--- for_post/nodejs-filesystem-read-file/s1-basics/basic.js
+++ for_post/nodejs-filesystem-read-file/s1-basics/basic.js
@@ -1,9 +1,6 @@
 let fs = require('fs');
 
 fs.readFile('./basic.js', (err, data) => {
-    if (err) {
-        console.log(err);
-    }
     console.log(Buffer.isBuffer(data)); // true
     console.log(typeof data); // 'object'
     console.log(data.toString()); // [text of this code]
