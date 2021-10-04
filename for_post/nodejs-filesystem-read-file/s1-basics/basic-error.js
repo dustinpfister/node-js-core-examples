6e3c30823476dc015ceb5098f9e7c6f65df43b54
dustinpfister <dustin.pfister@gmail.com>
2021-10-04 15:48:43 -0400

    basic error example looks good

--- for_post/nodejs-filesystem-read-file/s1-basics/basic-error.js
+++ for_post/nodejs-filesystem-read-file/s1-basics/basic-error.js
@@ -1,18 +1,26 @@
 let fs = require('fs'),
 uri_conf = './conf.json';
 
-let step = (obj)=> {
-	
-	
+let step = (obj) => {
+    obj.count += 1;
+    console.log('count is now: ' + obj.count);
+    fs.writeFile(uri_conf, JSON.stringify(obj), (e) => {
+        if (e) {
+            console.log(e.message)
+        } else {
+            console.log('updated conf.json');
+        }
+    })
 };
 
 fs.readFile(uri_conf, 'utf8', (err, data) => {
     if (err) {
         if (err.code === 'ENOENT') {
+            console.log('NO conf.json FILE, making a new one!');
             let newObj = {
                 count: 0
             };
-            fs.writeFile(uri_conf, JSON.stringify(newObj), function (e) {
+            fs.writeFile(uri_conf, JSON.stringify(newObj), (e) => {
                 if (!e) {
                     step(newObj);
                 }
