86275515cebd355d1b7ecb05b3fa8b48a79fac6e
dustinpfister <dustin.pfister@gmail.com>
2024-04-07 06:50:20 -0400

    simple y option to set eol

--- demos/20/crypto-create-cipher-iv/README.md
+++ demos/20/crypto-create-cipher-iv/README.md
@@ -4,12 +4,13 @@ I would like to make at least one if not more node v20 friendly scripts for the 
 
 ## The coder.js file
 
-```bash
-$ echo -n "this is what I want to encode" | node coder.js "password" "salt" "AES-256-CBC" "cipher"
-904a555508b44e92b039986b59b1ad427ff3e52ebbf6a9e28fe923444898488a
-```
+For the coder.js file I just wanted a simple script that I can use to cipher or decipher some text that I give to the script by way of standard input. This way I can use any linux command as a way to create the content to enctypt or to provide content to decrypt. For example when it comes to just working this out in the command line alone without creating any kind of file I count use the echo bash built in command.
 
 ```bash
-$ echo -n "904a555508b44e92b039986b59b1ad427ff3e52ebbf6a9e28fe923444898488a" | node coder.js "password" "salt" "AES-256-CBC" "decipher"
+$ echo -n "this is what I want to encode" | node coder.js "password" "salt" "AES-256-CBC" "cipher" "y"
+904a555508b44e92b039986b59b1ad427ff3e52ebbf6a9e28fe923444898488a
+$ echo -n "904a555508b44e92b039986b59b1ad427ff3e52ebbf6a9e28fe923444898488a" | node coder.js "password" "salt" "AES-256-CBC" "decipher" "y"
 this is what I want to encode
 ```
+
+I can then also use any
--- demos/20/crypto-create-cipher-iv/coder.js
+++ demos/20/crypto-create-cipher-iv/coder.js
@@ -1,23 +1,23 @@
 // Just using nodejs crypto module
 const crypto = require('crypto');
-
 // parse arguments
 const str_passwd = process.argv[2] || 'password';
 const str_iv = process.argv[3] || 'foobar';
 const str_a = process.argv[4] || 'AES-256-CBC';
 const str_mode = process.argv[5] || 'cipher';
-const str_format1 = process.argv[6] || 'hex';
-const str_format2 = process.argv[7] || 'utf8';
-const str_EOL = process.argv[8] === undefined ? '\n' : process.argv[8];
-
-console.log( Buffer.from(str_EOL) );
-
+const str_EOL_flag = process.argv[6] === undefined ? 'y' : process.argv[6];
+let str_EOL = '\n';
+if(str_EOL_flag != 'y'){
+    str_EOL = '';
+}
+// fortamts
+const str_format1 = 'hex';
+const str_format2 = 'utf8';
 // create key, iv, as well as cipher and decipher class instances
 const key = Buffer.concat([ Buffer.from(str_passwd) ], 32);
 const iv = Buffer.concat([ Buffer.from(str_iv) ], 16); 
 const cipher = crypto.createCipheriv( str_a, key, iv );
 const decipher = crypto.createDecipheriv(str_a, key, iv);
-
 // init output variable, attach events for cipher and decipher
 let str_out = '';
 decipher.on('readable', () => {
@@ -35,7 +35,7 @@ cipher.on('data', (data) => {
 cipher.on('end', () => {
     process.stdout.write( str_out + str_EOL );
 });
-    
+// process standard input   
 process.stdin.on('data', (data)=>{
     if(str_mode === 'decipher'){
         decipher.write(data.toString( str_format2 ), str_format1);
@@ -44,7 +44,6 @@ process.stdin.on('data', (data)=>{
         cipher.write(data.toString( str_format2 ), str_format2);
     }
 });
-
 process.stdin.on('end', (data)=>{
     if( str_mode === 'decipher' ){
         decipher.end();
@@ -53,6 +52,4 @@ process.stdin.on('end', (data)=>{
         cipher.end();
     }
 });
-
-
 
